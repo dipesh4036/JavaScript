44e20f33f02c27e5692cb329effd3171c110d652
dipesh4036 <111883699+dipesh4036@users.noreply.github.com>
2023-09-20 16:25:54 +0000

    main

--- class-1/conersion.js
+++ class-1/conersion.js
@@ -16,9 +16,76 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "Dipesh" => true
 
 
-console.log("1"+2);
-console.log(2+"1");
-console.log("1"+2+2);
-console.log(2+2+"1");
+// console.log("1"+2);
+// console.log(2+"1");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
 
-console.log(+true);
+// console.log(+true);
+
+console.log("2">1);
+
+console.log(null >0);
+console.log(null == 0);
+
+console.log(undefined >= 0);
+
+
+console.log("2"==2);
+console.log("2"===2);
+
+
+// Data Types & their return types:
+// _____________________________
+
+// 1. primitive (call by value):-
+
+// a) string => string
+// b) number => number
+// c) boolean => boolean
+// d) null => object
+// e) undefined => undefined
+// f) symbol => symbol
+// g) bigint => bigint
+
+// 2. non-primitive (reference type):-
+
+// a) array => object
+// b) object => object
+// c) function => function (function-object)
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
