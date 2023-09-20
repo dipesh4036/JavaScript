947ae308327366679220c207ae9f25716b465230
dipesh4036 <111883699+dipesh4036@users.noreply.github.com>
2023-09-20 12:29:31 +0000

    main

--- class-1/test.js
+++ class-1/test.js
@@ -1,1 +1,41 @@
-console.log("hello");
+const accountId = 123456 ;
+let accountEmail = "dipesh@gmail.com" ;
+var accountPassword = "20202020" ;
+accountcity = "jaipur" ;
+let accountCIF;  // gives undefind
+
+// accountId = 7890123 ;   can't cahnge
+accountEmail = "rahul@gmail.com" ;
+accountPassword = "10101010" ; 
+accountcity = "rajkot";
+
+
+// Can't Use Var in Code only use Let 
+
+console.table([accountId , accountEmail ,accountPassword ,accountcity ,accountCIF]);
+
+
+// ==> Data Types:- 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+let score = 32
+
+console.log(typeof score);
+
+let value = Number(score);
+console.log(typeof value);
+console.log(value);
